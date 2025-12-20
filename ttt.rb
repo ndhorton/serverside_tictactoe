3e7354c44e02c79c0ac5182707458d33a1a8de73
Nicholas Horton <nicholashorton7@protonmail.com>
2025-12-20 15:04:57 +0000

    Add API post route

--- ttt.rb
+++ ttt.rb
@@ -9,35 +9,9 @@ require 'yaml'
 
 require_relative 'lib/tttgame'
 
-# TODO: add final board state to `/game/over` views
-# * research Sinatra docs to see how to yield to the same end_state.erb sub-template
-# * yield to it from the end state ERB files
-
-configure do
-  enable :sessions
-  set :session_secret, SecureRandom.hex(64)
-end
-
-helpers do
-  def visible_space(marker)
-    marker.sub(/\s/, '&nbsp;')
-  end
-end
-
-get '/' do
-  redirect '/game/new'
-end
-
-get '/game/new' do
-  @game_state = TTTGame.new_game('X', :human, 'Sonny')
-  session[:game_state] = @game_state
-  erb :player_chooses
-end
-
-get '/game/over' do
-  @game_state = session[:game_state]
-  redirect '/game/new' if @game_state.nil?
-  board = TTTGame.deserialize_board(@game_state)
+def end_state_page(game_state)
+  @game_state = game_state
+  board = game_state[:board_state]
   if board.human_won?
     erb :layout, layout: false do
       erb :human_won do
@@ -59,30 +33,113 @@ get '/game/over' do
   end
 end
 
-get '/game/:human_choice' do
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(64)
+end
+
+helpers do
+  def visible_space(marker)
+    marker.sub(/\s/, '&nbsp;')
+  end
+end
+
+get '/' do
+  @game_state = TTTGame.new_game('X', :human, 'Sonny')
+  session[:game_state] = @game_state
+  erb :game
+end
+
+# get '/game/over' do
+#   @game_state = session[:game_state]
+#   redirect '/game/new' if @game_state.nil?
+#   board = TTTGame.deserialize_board(@game_state)
+#   if board.human_won?
+#     erb :layout, layout: false do
+#       erb :human_won do
+#         erb :end_state
+#       end
+#     end
+#   elsif board.computer_won?
+#     erb :layout, layout: false do
+#       erb :computer_won do
+#         erb :end_state
+#       end
+#     end
+#   else
+#     erb :layout, layout: false do
+#       erb :tie do
+#         erb :end_state
+#       end
+#     end
+#   end
+# end
+
+# get '/game/:human_choice' do
+#   @game_state = session[:game_state]
+#   redirect '/game/new' if @game_state.nil?
+#   board = TTTGame.deserialize_board(@game_state)
+#   @opponent = TTTGame.deserialize_opponent(@game_state)
+#
+#   # human moves
+#   board[params[:human_choice].to_i] = @game_state[:human_marker]
+#
+#   if board.end_state?
+#     @game_state[:board_state] = board.dump_board_state
+#     session[:game_state] = @game_state
+#     redirect '/game/over'
+#   end
+#
+#   # computer moves
+#   board[@opponent.choose(board)] = @game_state[:computer_marker]
+#
+#   # save current board state in the session game state
+#   # and instance variable for view
+#   @game_state[:board_state] = board.dump_board_state
+#   session[:game_state] = @game_state
+#
+#   redirect '/game/over' if board.end_state?
+#
+#   erb :player_chooses
+# end
+
+post '/game/:human_choice' do
   @game_state = session[:game_state]
   redirect '/game/new' if @game_state.nil?
   board = TTTGame.deserialize_board(@game_state)
-  @opponent = TTTGame.deserialize_opponent(@game_state)
 
   # human moves
   board[params[:human_choice].to_i] = @game_state[:human_marker]
 
   if board.end_state?
     @game_state[:board_state] = board.dump_board_state
-    session[:game_state] = @game_state
-    redirect '/game/over'
+
+    halt({ status: 'end',
+           content: end_state_page(@game_state) }.to_json)
   end
 
   # computer moves
-  board[@opponent.choose(board)] = @game_state[:computer_marker]
+  @opponent = TTTGame.deserialize_opponent(@game_state)
+  computer_move = @opponent.choose(board)
+  board[computer_move] = @game_state[:computer_marker]
 
-  # save current board state in the session game state
-  # and instance variable for view
+  if board.end_state?
+    @game_state[:board_state] = board.dump_board_state
+
+    halt({ status: 'end',
+           content: end_state_page(@game_state) }.to_json)
+  end
+
+  # save current board state in the session
   @game_state[:board_state] = board.dump_board_state
   session[:game_state] = @game_state
 
-  redirect '/game/over' if board.end_state?
-
-  erb :player_chooses
+  # Now we need to send back the computer's move and marker
+  # serialized as a JSON object
+  content_type :json
+  {
+    status: 'continue',
+    computer_move: computer_move,
+    computer_marker: @game_state[:computer_marker]
+  }.to_json
 end
