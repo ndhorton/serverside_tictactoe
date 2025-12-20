25c2c3ad3770f8512dd648c4eb5aeb11f9b0fdb5
Nicholas Horton <nicholashorton7@protonmail.com>
2025-12-20 18:46:40 +0000

    Add basic async and partial redraw functionality

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,66 @@
 // handle asynchronous requests and partial redraws
+
+// TODO: we need to prevent a situation where the player clicks more than
+// one square without waiting for computer turn
+
+// TODO: we are eventually going to need to set the player marker dynamically
+// and we will need to do so both here and on the server. Same goes for the
+// starting player turn
+
+const delayTime = 1000;
+
+let isHumanTurn = true;
+let humanMarker = 'X';
+
+$( document ).ready(function(){
+  $( '.square' ).click(function(event) {
+    const $clickedSquare = $(this);
+    if (!isHumanTurn || $clickedSquare.text().trim() !== '') {
+      return;
+    }
+    isHumanTurn = false;
+
+    // insert player marker in DOM
+    $clickedSquare.text(humanMarker);
+
+    // prevent user clicking on more squares until the computer opponent has
+    // moved
+
+    // send async request to handle move server-side
+    let squareNumber = $clickedSquare.data("position");
+    let t = (new Date()).getTime();
+    let request = $.ajax({
+      url: `/game/${squareNumber}`,
+      method: 'post',
+    });
+
+    request.done(function(data, textStatus, jqXHR) {
+      // I think this logic for checking for an end state
+      // might have to be more complex for situations where
+      // the computer moves and that results in an end state
+
+      let gameState = jqXHR['responseJSON'];
+      if (gameState['status'] == 'end') {
+        // if the human move ended the game, immediate redirect
+        window.location.replace('/game/over');
+        return;
+      }
+
+      let msEllapsed = (new Date()).getTime() - t;
+      let thinkTime = Math.max(0, msEllapsed);
+
+      // mark the computer opponent's marker after 'thinking time' pause
+      setTimeout( function() {
+          let newSquareNumber = gameState['computer_move']
+          let $targetSquare = $(`.square[data-position="${newSquareNumber}"]`);
+          $targetSquare.text(gameState['computer_marker']);
+          isHumanTurn = true;
+        },
+        thinkTime);
+
+      if (gameState['status'] == 'end_after') {
+        window.location.replace('/game/over');
+      }
+    });
+  });
+});
--- ttt.rb
+++ ttt.rb
@@ -9,30 +9,6 @@ require 'yaml'
 
 require_relative 'lib/tttgame'
 
-def end_state_page(game_state)
-  @game_state = game_state
-  board = game_state[:board_state]
-  if board.human_won?
-    erb :layout, layout: false do
-      erb :human_won do
-        erb :end_state
-      end
-    end
-  elsif board.computer_won?
-    erb :layout, layout: false do
-      erb :computer_won do
-        erb :end_state
-      end
-    end
-  else
-    erb :layout, layout: false do
-      erb :tie do
-        erb :end_state
-      end
-    end
-  end
-end
-
 configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(64)
@@ -54,30 +30,30 @@ get '/game' do
   erb :game
 end
 
-# get '/game/over' do
-#   @game_state = session[:game_state]
-#   redirect '/game/new' if @game_state.nil?
-#   board = TTTGame.deserialize_board(@game_state)
-#   if board.human_won?
-#     erb :layout, layout: false do
-#       erb :human_won do
-#         erb :end_state
-#       end
-#     end
-#   elsif board.computer_won?
-#     erb :layout, layout: false do
-#       erb :computer_won do
-#         erb :end_state
-#       end
-#     end
-#   else
-#     erb :layout, layout: false do
-#       erb :tie do
-#         erb :end_state
-#       end
-#     end
-#   end
-# end
+get '/game/over' do
+  @game_state = session[:game_state]
+  redirect '/' if @game_state.nil?
+  board = TTTGame.deserialize_board(@game_state)
+  if board.human_won?
+    erb :layout, layout: false do
+      erb :human_won do
+        erb :end_state
+      end
+    end
+  elsif board.computer_won?
+    erb :layout, layout: false do
+      erb :computer_won do
+        erb :end_state
+      end
+    end
+  else
+    erb :layout, layout: false do
+      erb :tie do
+        erb :end_state
+      end
+    end
+  end
+end
 
 # get '/game/:human_choice' do
 #   @game_state = session[:game_state]
@@ -109,7 +85,7 @@ end
 
 post '/game/:human_choice' do
   @game_state = session[:game_state]
-  redirect '/game/new' if @game_state.nil?
+  redirect '/' if @game_state.nil?
   board = TTTGame.deserialize_board(@game_state)
 
   # human moves
@@ -117,9 +93,9 @@ post '/game/:human_choice' do
 
   if board.end_state?
     @game_state[:board_state] = board.dump_board_state
-
-    halt({ status: 'end',
-           content: end_state_page(@game_state) }.to_json)
+    session[:game_state] = @game_state
+    content_type :json
+    halt({ status: 'end' }.to_json)
   end
 
   # computer moves
@@ -129,9 +105,9 @@ post '/game/:human_choice' do
 
   if board.end_state?
     @game_state[:board_state] = board.dump_board_state
-
-    halt({ status: 'end',
-           content: end_state_page(@game_state) }.to_json)
+    session[:game_state] = @game_state
+    content_type :json
+    halt({ status: 'end_after' }.to_json)
   end
 
   # save current board state in the session
