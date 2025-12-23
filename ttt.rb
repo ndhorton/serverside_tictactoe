4e53672e1712bb5f627d39d40dac01fae2b1c06e
Nicholas Horton <nicholashorton7@protonmail.com>
2025-12-23 18:07:28 +0000

    Finish basic users and passwords functionality

--- ttt.rb
+++ ttt.rb
@@ -8,15 +8,59 @@ require 'sinatra/reloader' if development?
 require 'tilt/erubi'
 require 'yaml'
 
-# TODO: write tests for existing funcitonality.
+# TODO: write tests for existing functionality.
 
 # TODO: add user signin and password database
-# * add views for signup and signin
-# * modify existing views for signout
-# * add routes for signin, signout and signup
-# * add password YAML db
+# * modify existing views for signout and new game
 
 require_relative 'lib/tttgame'
+
+def credentials_pathname
+  if ENV['RACK_ENV'] == 'test'
+    # rubocop:disable Style/ExpandPathArguments
+    File.expand_path('../test/users.yaml', __FILE__)
+    # rubocop:enable Style/ExpandPathArguments
+  else
+    # rubocop:disable Style/ExpandPathArguments
+    File.expand_path('../users.yaml', __FILE__)
+    # rubocop:enable Style/ExpandPathArguments
+  end
+end
+
+def credentials_valid?(username, password)
+  credentials = load_user_credentials
+
+  credentials.key?(username) &&
+    BCrypt::Password.new(credentials[username]) == password
+end
+
+def encrypt(password)
+  BCrypt::Password.create(password).to_s
+end
+
+def load_user_credentials
+  File.exist?(credentials_pathname) ? YAML.load_file(credentials_pathname) : {}
+end
+
+def require_user_signin
+  return if user_signed_in?
+
+  session[:message] = 'You must be signed in to do that.'
+  redirect '/'
+end
+
+def save_user_credentials(credentials)
+  File.write(credentials_pathname, YAML.dump(credentials))
+end
+
+def user_exists?(username)
+  credentials = load_user_credentials
+  credentials.key?(username)
+end
+
+def user_signed_in?
+  !!session[:username]
+end
 
 configure do
   enable :sessions
@@ -37,21 +81,79 @@ get '/home' do
   erb :home
 end
 
-get '/signup' do
+get '/users/signup' do
   erb :signup
 end
 
-get '/signin' do
+get '/users/signin' do
   erb :signin
 end
 
+post '/users/signup' do
+  username = params[:username].strip
+  password = params[:password]
+
+  if username.empty?
+    session[:message] = 'Username cannot be blank.'
+    erb :signup
+  elsif password.empty?
+    session[:message] = 'Password cannot be blank.'
+    @previous_username = username
+    erb :signup
+  elsif user_exists?(username)
+    session[:message] = 'A user with that name already exists. ' \
+                        'Please choose another.'
+    @previous_username = username
+    erb :signup
+  else
+    credentials = load_user_credentials
+    credentials[username] = encrypt(password)
+    save_user_credentials(credentials)
+    session[:message] = 'Account created.'
+    redirect '/'
+  end
+end
+
+post '/users/signin' do
+  username = params[:username].strip
+  password = params[:password]
+
+  if username.empty?
+    session[:message] = 'Username cannot be blank.'
+    erb :signin
+  elsif password.empty?
+    session[:message] = 'Password cannot be blank.'
+    @previous_username = username
+    erb :signin
+  elsif !credentials_valid?(username, password)
+    session[:message] = 'The username and password do not match.'
+    @previous_username = username
+    erb :signin
+  else
+    session[:username] = username
+    redirect '/'
+  end
+end
+
+get '/users/signout' do
+  require_user_signin
+
+  session[:username] = nil
+  session[:message] = 'You have been signed out.'
+  redirect '/'
+end
+
 get '/game' do
+  require_user_signin
+
   @game_state = TTTGame.new_game('X', :human, 'Hal')
   session[:game_state] = @game_state
   erb :game
 end
 
 get '/game/over' do
+  require_user_signin
+
   @game_state = session[:game_state]
   redirect '/' if @game_state.nil?
   board = TTTGame.deserialize_board(@game_state)
@@ -76,35 +178,9 @@ get '/game/over' do
   end
 end
 
-# get '/game/:human_choice' do
-#   @game_state = session[:game_state]
-#   redirect '/game/new' if @game_state.nil?
-#   board = TTTGame.deserialize_board(@game_state)
-#   @opponent = TTTGame.deserialize_opponent(@game_state)
-#
-#   # human moves
-#   board[params[:human_choice].to_i] = @game_state[:human_marker]
-#
-#   if board.end_state?
-#     @game_state[:board_state] = board.dump_board_state
-#     session[:game_state] = @game_state
-#     redirect '/game/over'
-#   end
-#
-#   # computer moves
-#   board[@opponent.choose(board)] = @game_state[:computer_marker]
-#
-#   # save current board state in the session game state
-#   # and instance variable for view
-#   @game_state[:board_state] = board.dump_board_state
-#   session[:game_state] = @game_state
-#
-#   redirect '/game/over' if board.end_state?
-#
-#   erb :player_chooses
-# end
-
 post '/game/:human_choice' do
+  require_user_signin
+
   @game_state = session[:game_state]
   redirect '/' if @game_state.nil?
   board = TTTGame.deserialize_board(@game_state)
